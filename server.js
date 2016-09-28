8f11c91f7cb3a9244626010f5114c0fecc0795f2
JyosthnaRoyal <gjyosthna71@gmail.com>
2016-09-28 18:33:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ function createTemplate(data){
     var heading=data.heading;
     var content=data.content;
 }
-var htmlTemplate =`
+var htmlTemplate = `
 <html>
  <head>
      <title>
@@ -65,7 +65,7 @@ var htmlTemplate =`
     </div>
  </body>
 </html>
-;
+`;
 return htmlTemplate;
 }
 
