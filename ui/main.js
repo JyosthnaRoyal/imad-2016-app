9ba734ee6905a375f9a26bdad66f7152541aea84
JyosthnaRoyal <gjyosthna71@gmail.com>
2016-10-05 00:31:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,7 @@
 console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML = 'HASURA';
+var img = document.getElementById('madi');
+img.onclick = function() {
+    img.style.marginleft = '200px';
+};
